2c8638b5764a5aedcc5d119ce25074105c99cd14
wilburrr90 <wilburrr90@gmail.com>
2017-01-06 09:55:00 -0500

    Connect to mongo and update stories dropdown

--- client/bundle.js
+++ client/bundle.js
@@ -46,6 +46,7 @@
 
 	const ws = document.getElementById('workspace');
 	const blurBtn = document.getElementById('blur-btn');
+	const storiesList = document.getElementById('stories-list');
 	let blurred = false;
 
 	blurBtn.addEventListener('click', blurClick);
@@ -59,6 +60,29 @@
 	  } 
 	  blurred = !blurred;
 	}
+
+	var xhttp = new XMLHttpRequest();
+	xhttp.onreadystatechange = function() {
+	  let myStories = [];
+	  if (this.readyState == 4 && this.status == 200) {
+	    myStories = JSON.parse(this.responseText);
+	  }
+	  
+	  myStories.forEach(s => {
+	    var newItem = document.createElement('a');
+	    newItem.innerHTML = s.name;
+	    newItem.className = 'dropdown-item';
+	    newItem.setAttribute('href', '#');
+	    newItem.addEventListener('click', () => {
+	      ws.value = s.text;
+	    })
+	    storiesList.appendChild(newItem);
+	  });
+	};
+
+	xhttp.open("GET", "/api/get-stories", true);
+	xhttp.send();
+
 
 /***/ }
 /******/ ]);
