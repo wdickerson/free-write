88a794b7db1fe01aa9c02d9d206da2c012ae7e53
wilburrr90 <wdickerson@outlook.com>
2017-05-09 15:43:31 -0400

    Add facebook authentication strategy

--- app.js
+++ app.js
@@ -1,58 +1,73 @@
-"use strict";
+'use strict';
 const express = require('express');
-const app = express();
-const bodyParser = require("body-parser");
+const session = require('express-session');
+const bodyParser = require('body-parser');
 const moment = require('moment');
 const MongoClient = require('mongodb').MongoClient;
 const fwc = require('./free-write-config');
+const passport = require('passport');
+const FacebookStrategy = require('passport-facebook').Strategy;
 
-const port = process.env.PORT || 3000;
-const connStr = fwc.connStr;
-
-let db;
-MongoClient.connect (connStr, function(err, myDb) {
-  if (err) console.log(err);
-  db = myDb;
-});
-
-app.use(express.static('client'));
+const app = express();
+app.use(express.static('client/dist/'));
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.post('/export', (req, res, next) => {
-  const filename = 'Free Write ' + moment().format('M-D-YY h.ma') + '.txt';
-  res.setHeader('Content-disposition', 'attachment; filename="' + filename);
-  res.setHeader('Content-type', 'text/plain');
+  res.attachment('Free Write ' + moment().format('M-D-YY h.ma') + '.txt');
   res.send(req.body['user-text']);
-})
-
-const router = express.Router();
-app.use('/api', router);
-
-router.get('/get-stories', (req, res, next) => {
-  getStories(db, res);
 });
 
-function getStories(db, res) {
-  if (db && db !== "null" && db !== "undefined") {
-    // get the stories from the stories collection
-    db.collection('stories').find({}, {limit:10, sort:[['_id', 'desc']]}, function(err, cursor) {
-      if (err) {
-        console.log(err.stack);
-      } else {
-        cursor.toArray(function(err, items) {
-          if (err) {
-            console.log(err.stack); 
-          } else {
-            res.json(items);
-          }
-        });
-      }
-    });     
-  } else {
-    console.log('No mongo found');
+app.get('/get-stories', (req, res) => {  
+  MongoClient.connect(fwc.connStr, (err, db) => {
+    if (err) return res.send('db error');
+    db.collection('stories').find({}, {limit:10, sort:[['_id', 'desc']]}, (err, cursor) => {
+      if (err) return res.send('nested db error');
+      cursor.toArray((err, items) => res.json(items));
+    });   
+  });
+});
+
+// Wip. Need to keep users in a mongo collection
+const users = [];
+
+const fbConfig = {
+  clientID: fwc.fbAppId,
+  clientSecret: fwc.fbAppSecret,
+  callbackURL: 'facebook-callback'
+};
+
+const fbStrategy = new FacebookStrategy(fbConfig, (access, refresh, profile, cb) => {
+  const myFbUser = users.find(u => u.fbid === profile.id); // Should query mongo
+  if (myFbUser) return cb(null, myFbUser);
+  const newFbUser = {
+    username: profile.displayName,
+    fbid: 1,
+    isUser: true,
   }
-}
-
-const server = app.listen(port, function () {
-  console.log('Server running at http://127.0.0.1:' + port + '/');
+  users.push(newFbUser); // Should insert to mongo
+  return cb(null, newFbUser);
 });
+
+passport.serializeUser((user, cb) => cb(null, user.username));
+passport.deserializeUser((username, cb) => cb(null, users.find(u => u.username === username)));
+passport.use(fbStrategy);
+
+app.use(session({ secret: fwc.sessionSecret }));
+app.use(passport.initialize());  
+app.use(passport.session());
+
+app.get('/login/facebook', passport.authenticate('facebook'));
+app.get('/login/facebook-callback', passport.authenticate('facebook', { 
+  successRedirect: '/', 
+  failureRedirect: '/fail' 
+}));
+
+app.get('/fail', (req, res) => res.send('unknown user'));
+app.get('/logout', (req, res) => req.logout() || res.redirect('/'));
+app.get('/authenticate', (req, res) => {
+  if (req.isAuthenticated()) return res.json({ isUser: true, name: req.user.username });
+  return res.json({ isUser: false, name: null });
+});
+
+const port = process.env.PORT || 3000;
+const server = app.listen(port, () => console.log('Running @ http://127.0.0.1:' + port));
--- client/free-write.js
+++ client/free-write.js
@@ -1,38 +1,39 @@
+const xhr = require('xhr');
 const ws = document.getElementById('workspace');
 const blurBtn = document.getElementById('blur-btn');
 const storiesList = document.getElementById('stories-list');
+const userHeader = document.getElementById('user-header');
+const displayName = document.getElementById('display-name');
+const guestHeader = document.getElementById('guest-header');
+
 let blurred = false;
-
-blurBtn.addEventListener('click', blurClick);
-
-function blurClick(e) {
-  ws.style.color = 'black';
-  ws.style.textShadow = 'none';
-  if (!blurred) {
-    ws.style.color = 'transparent';
-    ws.style.textShadow = '0 0 25px gray';
-  } 
+blurBtn.addEventListener('click', () => {
+  ws.style.color = blurred ? 'black' : 'transparent';
+  ws.style.textShadow = blurred ? 'none' : '0 0 25px gray';
   blurred = !blurred;
-}
+});
 
-var xhttp = new XMLHttpRequest();
-xhttp.onreadystatechange = function() {
-  let myStories = [];
-  if (this.readyState == 4 && this.status == 200) {
-    myStories = JSON.parse(this.responseText);
-  }
-  
-  myStories.forEach(s => {
-    var newItem = document.createElement('a');
+xhr({
+  uri: '/get-stories',
+  json: true,
+}, (err, resp, body) => {
+  body.forEach(s => {
+    const newItem = document.createElement('a');
     newItem.innerHTML = s.name;
     newItem.className = 'dropdown-item';
-    newItem.setAttribute('href', '#');
-    newItem.addEventListener('click', () => {
-      ws.value = s.text;
-    })
+    newItem.addEventListener('click', () => ws.value = s.text);
     storiesList.appendChild(newItem);
-  });
-};
+  });  
+});
 
-xhttp.open("GET", "/api/get-stories", true);
-xhttp.send();
+xhr({
+    uri: '/authenticate',
+    json: true,
+}, (err, resp, body) => {
+    if (body.isUser) {
+      userHeader.style.display = 'block';
+      displayName.innerHTML = body.name;
+    } else {
+      guestHeader.style.display = 'block';
+    }
+})
